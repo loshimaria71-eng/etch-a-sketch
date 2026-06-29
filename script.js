b68bd4e9b7e724df1e9883f2fa415b0cdc2c185a
Maria <loshimaria71@gmail.com>
2026-06-29 23:33:42 +0200

    Add button to resize grid with 100 square limit

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 const container = document.querySelector('.container');
+const button = document.querySelector('button');
 
-for (let i = 0; i < 256; i++) {
-    const square = document.createElement('div');
-    container.appendChild(square);
-    square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = 'black';
-    });
+function createGrid(size) {
+    for (let i = 0; i < size * size; i++) {
+        const square = document.createElement('div');
+        container.appendChild(square);
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = 'black';
+        });
+    }
 }
+
+button.addEventListener('click', () => {
+    let size = prompt('Sa katror per ane (max 100)?');
+    if (size > 100) {
+        alert('Maksimumi eshte 100!');
+        return;
+    }
+    container.innerHTML = '';
+    createGrid(size);
+});
+
+createGrid(16);

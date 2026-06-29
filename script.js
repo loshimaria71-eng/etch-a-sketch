b0298dbf20b87e5614916ad6afd66bb8005aa378
Maria <loshimaria71@gmail.com>
2026-06-29 18:15:24 +0200

    Add 16x16 grid with flexbox and hover effect

---
+++ script.js
@@ -0,0 +1,9 @@
+const container = document.querySelector('.container');
+
+for (let i = 0; i < 256; i++) {
+    const square = document.createElement('div');
+    container.appendChild(square);
+    square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = 'black';
+    });
+}
